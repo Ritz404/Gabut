ef58f7c2810eb8d7bc725a05708bc71c36461cde
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2025-01-21 18:30:18 +0700

    Update Genres.jsx

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -8,7 +8,7 @@ const Genres = () => {
         <div>
             <div className="flex flex-col items-center justify-center p-2">
                 <div className="flex text-4xl font-bold">
-                    <span className="">Zero</span>
+                    <span className="">Shiro</span>
                     <span className="text-blue-600">Nime</span>
                 </div>
                 <span className="text-xs -mt-2">Baca Komik Bahasa Indonesia</span>
